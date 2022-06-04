37b422a49a531a8144051bb3e57aa2ca2ec2f4d8
zhangshuyang <zhangshuyang@outlook.com>
2022-06-05 00:37:19 +0800

    feat: added a proxy to get audio source of bilibili video

--- app/music/bilibili/search.py
+++ app/music/bilibili/search.py
@@ -1,9 +1,10 @@
-from nis import match
 import aiohttp
 
 
 BILIBILI_VIDEO_INFO_API = "http://api.bilibili.com/x/web-interface/view"
 BILIBILI_AUDIO_SOURCE_API = "https://api.bilibili.com/x/player/playurl"
+
+BPROXY_API = "https://bproxy.shuyangzhang.repl.co/bproxy"
 
 
 async def fetch_basic_video_info_by_BVid(BVid: str):
@@ -33,7 +34,8 @@ async def fetch_basic_video_info_by_BVid(BVid: str):
     return matched, name, author, cid, duration
 
 async def fetch_audio_source_by_BVid_and_cid(BVid: str, cid: int):
-    url = f"{BILIBILI_AUDIO_SOURCE_API}?qn=16&fnval=80&bvid={BVid}&cid={cid}"
+    url = f"{BILIBILI_AUDIO_SOURCE_API}?bvid={BVid}&cid={cid}&qn=16&fnval=80"
+
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as r:
             resp_json = await r.json()
@@ -66,7 +68,33 @@ async def bvid_to_music(BVid: str):
     print(matched, name, author, source, duration)
     return matched, name, author, source, duration
 
+async def bvid_to_music_by_bproxy(BVid: str):
+    url = f"{BPROXY_API}?bvid={BVid}"
+    
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as r:
+            resp_json = await r.json()
+            status = resp_json.get("status", {}).get("status_code", 500)
+            if status == 500:
+                raise Exception(resp_json.get("status", {}).get("msg", "fetch bproxy failed, unknown reason."))
+            else:
+                data = resp_json.get("data", {})
+                if data:
+                    matched = True
+                    name = data.get("name", "")
+                    author = data.get("author", "")
+                    source = data.get("source")
+                    duration = data.get("duration", 180000)
+                else:
+                    matched = False
+                    name = ""
+                    author = ""
+                    source = ""
+                    duration = 0
+    print(matched, name, author, source, duration)
+    return matched, name, author, source, duration
+
 if __name__ == "__main__":
     import asyncio
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(bvid_to_music("BV1Jb411U7u2"))
+    loop.run_until_complete(bvid_to_music_by_bproxy("BV1Jb411U7u2"))
